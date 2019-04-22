0be7248e75b4c487fb54dac668ae11860d595551
neroark <ns_ark@hotmail.com>
2019-04-22 21:35:16 +0200

    feat: add store + dev tool

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import sucessreducer from "./successreducer";
+
+const reducer = combineReducers({ sucessreducer });
+
+export default reducer;
---
+++ src/store/reducers/successreducer.js
@@ -0,0 +1,7 @@
+const initialState = false;
+
+const successreducer = (state = initialState, action) => {
+  return state;
+};
+
+export default successreducer;
